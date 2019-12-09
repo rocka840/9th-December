27815f4c2612745799ebb9db921343170c6b7d5d
unknown <RocKa840@ltpemdomain.local>
2019-12-09 09:44:36 +0100

    hhhhh

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-document.getElementById("makeItWork").addEventListener(enableClick);
+document.getElementById("makeItWork").addEventListener("contextmenu", enableClick);
+document.getElementById("makeItWork").addEventListener("click", disableClick);
 
 function enableClick() {
 
     event.preventDefault();
     //alert("you just turned on the clicking on the other button");
-    document.getElementById("clickMe").addEventListener("contextmenu", startClick); //contextmenu for right-click, click for left-click
+    document.getElementById("clickMe").addEventListener("click", startClick); //contextmenu for right-click, click for left-click
     document.getElementById("makeItWork").classList.add("enabledButton");
 }
+
+function disableClick() {
+
+    event.preventDefault();
+    //alert("you just turned on the clicking on the other button");
+    document.getElementById("clickMe").removeEventListener("click", startClick); //contextmenu for right-click, click for left-click
+    document.getElementById("makeItWork").classList.remove("enabledButton");
+}
+
+
 
 function startClick() {
     alert("hi");
